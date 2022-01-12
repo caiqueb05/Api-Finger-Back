c7ce905374d41a50842d3526224363588bf6f03e
caiqueb05 <caique.bezerra@gmail.com>
2022-01-12 16:42:17 -0300

    [Task: configuraçao sqlite]

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Users } from './users/entities/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      host: join(__dirname, 'database.sqlite'),
+      autoLoadModels: true,
+      models: [Users],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
